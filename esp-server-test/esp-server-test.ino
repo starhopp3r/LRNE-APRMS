6348cc74b83dd96d7e46a5e9a237738c0ea3bd5a
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2018-11-28 08:49:51 +0800

    refactored some code

--- esp-server-test/esp-server-test.ino
+++ esp-server-test/esp-server-test.ino
@@ -3,20 +3,23 @@
 #include <ArduinoJson.h>
  
 void setup() {
- 
-  Serial.begin(115200);                            //Serial connection
-  WiFi.begin("Orange", "navnik18");   //WiFi connection
- 
-  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+  // Serial connection
+  Serial.begin(115200);
+  // Credentials for WiFi connection
+  WiFi.begin("xxxxx", "xxxxx");
+
+  // Wait for the WiFI connection completion
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Waiting for connection");
   }
 }
  
 void loop() {
- 
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
+  // Check WiFi connection status
+  if (WiFi.status() == WL_CONNECTED) {
+    // Declaring static JSON buffer
+    StaticJsonBuffer<300> JSONbuffer;
     JsonObject& JSONencoder = JSONbuffer.createObject(); 
  
     JSONencoder["sensorType"] = "Temperature";
@@ -34,24 +37,30 @@ void loop() {
     char JSONmessageBuffer[300];
     JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
     Serial.println(JSONmessageBuffer);
- 
-    HTTPClient http;    //Declare object of class HTTPClient
- 
-    http.begin("http://192.168.1.128:5000/dev");      //Specify request destination
-    http.addHeader("Content-Type", "application/json");  //Specify content-type header
- 
-    int httpCode = http.POST(JSONmessageBuffer);   //Send the request
-    String payload = http.getString();                                        //Get the response payload
- 
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(payload);    //Print request response payload
- 
-    http.end();  //Close connection
- 
+
+    // Declare object of class HTTPClient
+    HTTPClient http;
+
+    // Specify request destination
+    http.begin("http://local-ip-address:5000/dev");
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/json");
+    // Send the request
+    int httpCode = http.POST(JSONmessageBuffer);
+    // Get the response payload
+    String payload = http.getString();
+    // Print HTTP return code
+    Serial.println(httpCode);
+    // Print request response payload
+    Serial.println(payload);
+
+    // Close connection
+    http.end();
   } else {
     Serial.println("Error in WiFi connection");
   }
-  delay(30000);  //Send a request every 30 seconds
- 
+
+  // Send a request every 30 seconds
+  delay(30000);
 }
 
