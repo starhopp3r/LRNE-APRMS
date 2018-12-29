bb6b69b1e0f31d83078e12f963671fbb62ce2889
Bryan Tee Pak Hong <bryantee1998@gmail.com>
2018-12-29 15:30:54 +0800

    These files uses software serial to transmit data
    Issue: Software Serial only can read 1 character, making the updating of handset from loPy not possible (saving state of Handset not possible).

---
+++ HADR_receivePayload/HADR_receivePayload.ino
@@ -0,0 +1,60 @@
+// LoPy placeholder: Arduino MEGA
+// LoPy receive code
+#include <SoftwareSerial.h>
+
+/*JSON object SEND from handset
+   {
+    "user":"",
+    "latitude":"",
+    "longitude:""
+   }
+*/
+
+// ==============================Some parser codes==============================
+String parseMessage(String rawMessage, byte index) {
+  if (rawMessage.substring("{") != 0) {
+    Serial.println(rawMessage);
+    return "Object is not json!";
+  }
+  byte ind;
+  if (index == 0) {
+    ind = rawMessage.indexOf("user");
+    ind += 4;
+  }
+  else if (index == 1) {
+    ind = rawMessage.indexOf("latitude");
+    ind += 8;
+  }
+  else if (index == 2) {
+    ind = rawMessage.indexOf("longitude");
+    ind += 9;
+  }
+  ind += 3;
+  return String(rawMessage.substring(ind, rawMessage.indexOf('"', ind)));
+}
+//======================================end===============================
+
+SoftwareSerial handset (11,10);
+String payload;
+void setup() {
+  Serial.begin(9600);
+  handset.begin(9600); 
+}
+
+void loop() {
+  payload = "";
+  handset.println(payload);
+  handset.listen();
+  while(handset.available()){
+    char c = handset.read();
+    payload += c;
+  }
+  Serial.println(payload);
+  Serial.print("user:      ");Serial.println(parseMessage(payload,0));
+  Serial.print("latitude:  ");Serial.println(parseMessage(payload,1));
+  Serial.print("longitude: ");Serial.println(parseMessage(payload,2));
+  delay(2000);
+
+}
+
+
---
+++ HADR_sendPayload/HADR_sendPayload.ino
@@ -0,0 +1,42 @@
+// Handset sending code
+#include <SoftwareSerial.h>
+
+SoftwareSerial loPy(13,12);
+unsigned long screenTime = millis();
+float flat = 0;
+float flon = 0;
+String payload = "";
+
+// ==============================Some parser codes==============================
+String encodeMessage(String message) {
+  // NOTE: USER will not send back broadcast message!!!
+  String buffer = "{";
+  buffer += "\"user\":\"" + message + "\"";
+  buffer += "\"latitude\"" + String(flat) + "\",";
+  buffer += "\"longitude\"" + String(flon) + "\"";
+  buffer += "}";
+  return buffer;
+}
+//======================================end===============================
+
+void setup() {
+  Serial.begin(9600);
+  loPy.begin(9600);
+  Serial.println("Ready!");
+  while(!loPy.available());
+  loPy.listen();
+  /*while(loPy.available()){
+    char c = loPy.read();
+    payload += c;
+  }
+  Serial.println(payload);
+  Serial.print("user: ");Serial.println(parseMessage(payload,0));
+  Serial.print("broadcast: ");Serial.println(parseMessage(payload,1));
+  delay(2000);*///suspect arduino has some limitations for reading and writing form the same port
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (!((millis() - screenTime) > 5000)); //waits every 5 seconds
+  loPy.print(encodeMessage("Shelter"));
+}
