344462b9baad8926abae551620c43a3383a2fe3a
James <lim.en.hui.james@gmail.com>
2018-12-30 20:55:43 +0800

    LoPy Various: Mesh Networking stability improved

--- LoPy - Operator/main.py
+++ LoPy - Operator/main.py
@@ -7,11 +7,11 @@
 
 from network import LoRa, WLAN
 from loramesh import Loramesh
-import gc
 import machine
 import socket
 import pycom
-import utime
+import time
+import gc
 
 # Constants
 WIFI_SSID = "<wifi ssid here>"
@@ -20,6 +20,12 @@ POST_IP = "<dashboard ip here>"
 POST_PORT = 5000
 
 PORT = 1000
+
+# Blink function. yes.
+def blink(color):
+    pycom.rgbled(color)
+    time.sleep_ms(100)
+    pycom.rgbled(0x000000)
 
 # Function to print changes in strings
 old = ""
@@ -35,8 +41,10 @@ def http_post(addr, port, data):
     __data = bytes("POST /inlet HTTP/1.1\r\nHost: {:s}:{:d}\r\nContent-Type: text/plain\r\nContent-Length: {:d}\r\n\r\n".format(addr, port, len(data)), "utf8") + data # data is already bytes
     tcp_s.connect(socket.getaddrinfo(addr, port)[0][-1])
     tcp_s.send(__data)
-    tcp_s.setblocking(True)
-    ok_message = tcp_s.recv(256)
+    ok_message = b""
+    while ok_message == b"" or ok_message[-1] != 35:
+        ok_message += tcp_s.recv(128)
+    print("[Debug - HTTP Post] Response: %s" % ok_message)
     tcp_s.close()
     return strip_headers(ok_message)
 
@@ -67,6 +75,9 @@ print("[Debug] Initializing socket")
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.bind(PORT)
 
+# Turn off heartbeat
+pycom.heartbeat(False)
+
 # Begin socializing with other neighbors and perform transactions with data
 neighbors = []
 ip = mesh.ip()
@@ -74,42 +85,60 @@ if __name__ == "__main__":
     buf = ""
 
     while True:
-        # Check if this particular node has changed IP addresses
-        if not ip == mesh.ip():
-            ip = mesh.ip()
-            neighbors = [] # if there was an IP change, inform everybody
+        try:
+            # Check if this particular node has changed IP addresses
+            if not ip == mesh.ip():
+                ip = mesh.ip()
+                neighbors = [] # if there was an IP change, inform everybody
 
-        # Check the neighbors list, and submit our IP to them.
-        new_neighbors = mesh.neighbors_ip()
-        diff_neighbors = set(new_neighbors)- set(neighbors)
-        u_print("[Debug] Neighbors not acknowledged: %s" % diff_neighbors)
-        for neighbor in diff_neighbors:
-            if mesh.ping(neighbor) > 0:
+            # Check the neighbors list, and submit our IP to them.
+            new_neighbors = mesh.neighbors_ip()
+            diff_neighbors = set(new_neighbors)- set(neighbors)
+            u_print("[Debug] Neighbors not acknowledged: %s" % diff_neighbors)
+            for neighbor in diff_neighbors:
                 s.sendto("MASTER%s" % (ip), (neighbor, PORT))
+                time.sleep(3)
 
-        # Remove any dropped nodes
-        neighbors = list(set(neighbors) - (set(neighbors) - set(new_neighbors)))
-        gc.collect() # run garbage collection
+            # Remove any dropped nodes
+            neighbors = list(set(neighbors) - (set(neighbors) - set(new_neighbors)))
 
-        # There are three possible messages: MASTER, MSG and ACK. Beacuse we are the master, ignore MASTER messages
-        buf, recv_addr = s.recvfrom(512)
-        if len(buf) > 0 and buf != "":
-            print("[Debug] Packet recieved from %s: %s" % (recv_addr[0], buf))
+            # There are three possible messages: MASTER, MSG and ACK. Beacuse we are the master, ignore MASTER messages
+            buf, recv_addr = s.recvfrom(64)
+            if len(buf) > 0 and buf != "":
+                print("[Debug] Packet recieved from %s: %s" % (recv_addr[0], buf))
 
-            if buf.startswith("MSG"):
-                buf = buf[3:]
-                print("[Debug - Recieved Packet] It was a message: %s" % (buf))
-                # Verify the buffer
-                if buf.count(b",") != 4:
-                    continue # we haven't recieved the full buffer
+                if buf.startswith("MSG"):
+                    blink(0x00FF00)
+                    buf = buf[3:]
+                    print("[Debug - Recieved Packet] It was a message: %s" % (buf))
+                    # Verify the buffer
+                    if buf.count(b",") != 4:
+                        continue # we haven't recieved the full buffer
 
-                # Send the buffer to the server
-                ok_message = http_post(POST_IP, POST_PORT, buf)
+                    # Send the buffer to the server
+                    ok_message = http_post(POST_IP, POST_PORT, buf)
 
-                # Transmit the ok message back to the slave
-                if mesh.ping(recv_addr[0]) > 0:
+                    # Transmit the ok message back to the slave
                     s.sendto("MSG" + ok_message.decode(), recv_addr)
-            elif buf.startswith("ACK"):
-                buf = buf[3:]
-                print("[Debug - Recieved Packet] It was an acknowledgement for neighbor discovery, from: %s" % (buf.decode()))
-                neighbors.append(buf.decode())
+                    time.sleep(3)
+                elif buf.startswith("ACK"):
+                    blink(0x0000FF)
+                    buf = buf[3:]
+                    print("[Debug - Recieved Packet] It was an acknowledgement for neighbor discovery, from: %s" % (buf.decode()))
+                    neighbors.append(buf.decode())
+        except OSError as e:
+            print("[Error] An error occured while attempting something. Exact error (no. %s): %s" % (e.errno, e))
+            
+            if e.errno == "Avialable Interfaces are down":
+                print("[Error] The error has been documented in issue #15. Reconnecting WiFi...")
+                blink(0xFF69B4)
+                networks = wlan.scan()
+                for network in networks:
+                    if network.ssid == WIFI_SSID:
+                        wlan.connect(network.ssid, auth=(network.sec, WIFI_PASSWORD), timeout=5000)
+                        while not wlan.isconnected(): continue
+
+            if e.errno == 12:
+                print("[Error] The error has been documented in issue #15. Triggering garbage collector and sleeping for 6 seconds. Current free memory: %s" % (gc.mem_free()))
+                gc.collect()
+                time.sleep(6)
--- LoPy - Testing Scripts/slave_mesh_test.py
+++ LoPy - Testing Scripts/slave_mesh_test.py
@@ -4,10 +4,11 @@
 from network import LoRa
 from machine import UART
 from loramesh import Loramesh
-import gc
 import socket
 import time
 import machine
+import pycom
+import gc
 
 TX_PIN = "G17"
 RX_PIN = "G22"
@@ -21,6 +22,12 @@ def u_print(new):
     if new != old:
         print(new)
         old = new
+
+# A blink function.
+def blink(color):
+    pycom.rgbled(color)
+    time.sleep_ms(100)
+    pycom.rgbled(0x000000)
 
 print("Warning: You are in a testing script. Please use the production version of the script, if available.")
 print("[Debug] Initializing LoRa")
@@ -37,51 +44,71 @@ s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.bind(PORT)
 print("[Debug] Socket initialized")
 
+# Turn off heartbeat
+pycom.heartbeat(False)
+
 neighbors = mesh.neighbors_ip() # first neighbor already knows the master
 ip = mesh.ip()
 master_ip = ""
 while True:
     buf = ""
 
-    # React to any IP change
-    if not ip == mesh.ip():
-        ip = mesh.ip()
+    try:
+        # React to any IP change
+        if not ip == mesh.ip():
+            ip = mesh.ip()
 
-    # Check for any incoming messages: MASTER, MSG and ACK
-    buf, recv_addr = s.recvfrom(512)
-    if len(buf) > 0 and buf != "":
-        print("[Debug] Packet recieved from %s: %s" % (recv_addr[0], buf))
+        # Check for any incoming messages: MASTER, MSG and ACK
+        buf, recv_addr = s.recvfrom(64)
+        if len(buf) > 0 and buf != "":
+            print("[Debug] Packet recieved from %s: %s" % (recv_addr[0], buf))
 
-        if buf.startswith("MASTER"):
-            buf = buf[6:]
-            print("[Debug - Recieved Packet] It was a MASTER IP information packet: %s" % (buf))
-            master_ip = buf.decode()
-            if mesh.ping(recv_addr[0]) > 0:
+            if buf.startswith("MASTER"):
+                blink(0xFF0000)
+                buf = buf[6:]
+                print("[Debug - Recieved Packet] It was a MASTER IP information packet: %s" % (buf))
+                master_ip = buf.decode()
                 s.sendto("ACK" + ip, recv_addr)
-        elif buf.startswith("MSG"):
-            buf = buf[3:]
-            print("[Debug - Recieved Packet] It was a message: %s" % (buf))
-            # TODO: Serial
-        elif buf.startswith("ACK"):
-            buf = buf[3:]
-            print("[Debug - Recieved Packet] It was an acknowledgement for neighbor discovery, from: %s" % (buf.decode()))
-            neighbors.append(buf.decode())
+                time.sleep(10)
+            elif buf.startswith("MSG"):
+                blink(0x00FF00)
+                buf = buf[3:]
+                print("[Debug - Recieved Packet] It was a message: %s" % (buf))
+                # TODO: Serial
+            elif buf.startswith("ACK"):
+                blink(0x0000FF)
+                buf = buf[3:]
+                print("[Debug - Recieved Packet] It was an acknowledgement for neighbor discovery, from: %s" % (buf.decode()))
+                neighbors.append(buf.decode())
 
-    # Check the neighbors list, and submit the master IP to them, if we have the master IP.
-    new_neighbors = mesh.neighbors_ip()
-    if not master_ip == "":
-        diff_neighbors = set(new_neighbors) - set(neighbors)
-        u_print("[Debug] Neighbors not acknowledged: %s" % diff_neighbors)
-        for neighbor in diff_neighbors:
-            if not neighbor == master_ip and mesh.ping(neighbor) > 0:
-                s.sendto("MASTER%s" % (ip), (neighbor, PORT))
+        # Check the neighbors list, and submit the master IP to them, if we have the master IP.
+        new_neighbors = mesh.neighbors_ip()
+        if not master_ip == "":
+            diff_neighbors = set(new_neighbors) - set(neighbors)
+            u_print("[Debug] Neighbors not acknowledged: %s" % diff_neighbors)
+            for neighbor in diff_neighbors:
+                if not neighbor == master_ip:
+                    s.sendto("MASTER%s" % (ip), (neighbor, PORT))
+                    time.sleep(5)
 
-    # Remove any dropped nodes
-    neighbors = list(set(neighbors) - (set(neighbors) - set(new_neighbors)))
-    gc.collect() # run garbage collection
+        # Remove any dropped nodes
+        neighbors = list(set(neighbors) - (set(neighbors) - set(new_neighbors)))
 
-    # NOTE: Only in the testing script
-    # If master_ip exists, and ping is successful, spam them with information
-    if not master_ip == "" and mesh.ping(master_ip) > 0:
-        print("[Debug] Master IP exists. Sending test data")
-        s.sendto("MSG0,16/12/18 11:08,51.174693,-3.061574,11", (master_ip, PORT)) # otherwise, sends the data, omitting the \n character
+        # NOTE: Only in the testing script
+        # If master_ip exists, and ping is successful, spam them with information
+        if not master_ip == "":
+            print("[Debug] Master IP exists. Sending test data")
+            blink(0xFFFFFF)
+            s.sendto("MSG0,16/12/18 11:08,51.174693,-3.061574,11", (master_ip, PORT)) # otherwise, sends the data, omitting the \n character
+            time.sleep(10)
+
+    except OSError as e:
+        print("[Error] An error ocurred while attempting something. Exact error (no. %d): %s" % (e.errno, e))
+        if e.errno == "106":
+            print("[Error] The error has been documented in issue #15. Neighbors: %s" % (neighbors))
+            continue
+
+        if e.errno == 12:
+            print("[Error] The error has been documented in issue #15. Triggering garbage collector and sleeping for 6 seconds. Current Mem: %s" % (gc.mem_free()))
+            gc.collect()
+            time.sleep(6)
